ab5f1dd177bd1985891cbeae3b2961ef8a716fd8
rosiemay341 <rosie.may@hotmail.com>
2026-03-07 18:31:00 +0000

    Add files via upload
    PWA WORKING WITH HTTPS Git/io

--- service-worker.js
+++ service-worker.js
@@ -2,21 +2,21 @@
 // FORCE UPDATE + DELETE OLD CACHES
 // ------------------------------------------------------------
 self.addEventListener("install", event => {
-  self.skipWaiting();
+    self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
-    )
-  );
-  self.clients.claim();
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.map(key => caches.delete(key)))
+        )
+    );
+    self.clients.claim();
 });
 
 // ------------------------------------------------------------
 // ALWAYS FETCH FRESH FILES (no caching)
 // ------------------------------------------------------------
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+    event.respondWith(fetch(event.request));
 });
